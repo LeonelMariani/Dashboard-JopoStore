7ef7c5a3f15ae9f32fb295e38040e963865d5250
marcelabus <guidoymarcela@hotmail.com>
2022-10-25 16:19:40 -0300

    componentes varios

--- src/components/ContentRowTop.js
+++ src/components/ContentRowTop.js
@@ -1,41 +1,19 @@
-import { React, useEffect, useState } from 'react';
+import React from 'react';
 import ContentRowCenter from './ContentRowCenter';
 import ContentRowQuantity from './ContentRowQuantity';
 import LastProductInDb from './LastProductInDb';
 import CategoriesInDb from './CategoriesInDb';
 import Chart from './Chart';
-import Search from './Search';
+// import Search from './Search';
 import ProductDetail from './ProductDetail';
 import NotFound from './NotFound'
 import { Route, Switch } from "react-router-dom";
 
 
-function ContentRowTop(){
-
-	const [products, setProducts] = useState ( [] );
-	const [lastProduct, setLastProduct] = useState ( {} );
-
-    useEffect( () => {
-
-		fetch ( 'http://localhost:3040/api/products/' )
-            .then ( response => response.json() )
-            .then ( data => {
-                setProducts (data.data.products);
-	            } )
-			.catch ( error => console.log (error))
-	}, [] );
-
-	useEffect ( () => {
-		// obtener el máximo id, que es el del último producto ingresado. Luego obtener el lastProduct
-		let maxId = Math.max.apply ( Math, products.map ( product => {return product.id} ));
-	
-		setLastProduct( products.find ( product => product.id = maxId) );
-
-	}, [products]);
-
-
+function ContentRowTop ( props ) {
+console.log (props.nombre);
 	return(
-				
+		<React.Fragment>	
 				<div className="container-fluid">
 					<div className="d-sm-flex aligns-items-center justify-content-between mb-4">
 						<h1 className="h3 mb-0 text-gray-800">Panel App</h1>
@@ -43,17 +21,25 @@ function ContentRowTop(){
 				
 					<Switch>
 						<Route path="/totales" component={ContentRowQuantity} />
-						<Route path="/ultimoProducto" component={LastProductInDb} />
+						{/* <Route path="/ultimoProducto" component={LastProductInDb} /> */}
+
+						<Route path="/ultimoProducto" >
+							<LastProductInDb />
+						</Route> 
+
 						<Route path="/categorias" component={CategoriesInDb} />
 						<Route path="/listaDeproductos" component={Chart} />
-						<Route path="/buscador" component={Search} />
+						{/* <Route path="/buscador" component={Search} /> */}
 						<Route path="/detalleDeProducto" component={ProductDetail} />
-						<Route exact path="/" component={ContentRowCenter} />
+						{/* <Route exact path="/" component={ContentRowCenter} /> */}
+						<Route exact path="/">
+							<ContentRowCenter />
+						</Route> 
 						<Route component={NotFound} />
 					</Switch>
 	
 				</div>
-	     
+		</React.Fragment>	 
     )
 
 }
--- src/components/LastProductInDb.js
+++ src/components/LastProductInDb.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import imagenFondo from '../assets/images/tocadiscos-4.jpg';
+// import imagenFondo from '../assets/images/tocadiscos-4.jpg';
 import {Link} from 'react-router-dom';
 
-function LastProductInDb(){
+function LastProductInDb () {
 
-    
-    return(
-        <div className="col-lg-6 mb-4">
-            <div className="card shadow mb-4">
-                <div className="card-header py-3">
-                    <h5 className="m-0 font-weight-bold text-gray-800">Último producto</h5>
-                </div>
-                <div className="card-body">
-                    <div className="text-center">
-                        <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 40 +'rem'}} src={imagenFondo} alt=" último producto "/>
+    let lastProduct = {};
+  
+    fetch ( 'http://localhost:3040/api/products/' )
+        .then ( response => response.json() )
+        .then ( data => {
+            return data.data.products;
+        } )
+        .then ( products => {
+            let maxId = Math.max.apply ( Math, products.map ( product => {return product.id} ))
+            let last = products.find ( product => product.id === maxId );
+            console.log (products);
+            console.log (last);        
+	
+            return last;
+        })
+        .then (last => {
+            return lastProduct = last;
+
+        })
+        .catch ( error => console.log (error))
+   console.log (lastProduct) 
+
+        return(
+            <div className="col-lg-6 mb-4">
+                <div className="card shadow mb-4">
+                    <div className="card-header py-3">
+                        <h5 className="m-0 font-weight-bold text-gray-800">Último producto</h5>
                     </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-                    <Link  to="/detalleDeProducto" className="btn btn-danger" rel="nofollow">Detalle de producto</Link>
+                    <div className="card-body">
+                        <div className="text-center">
+                            <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 40 +'rem'}} src={lastProduct.img} alt=" último producto "/>
+                        </div>
+                        <p>{lastProduct.description}</p>
+                        <Link  to="/detalleDeProducto" className="btn btn-danger" rel="nofollow">Detalle de producto</Link>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
-}
+        )
+
+    }
 
 export default LastProductInDb;
